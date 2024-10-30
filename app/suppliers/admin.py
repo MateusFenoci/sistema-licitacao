e0a9a1c95a9e3af22ba98194c1926d86b24294d2
Mateus Rezende Fenoci <mateusfenoci@MacBook-Air-de-Mateus-2.local>
2024-10-29 21:04:41 -0300

    Add CNPJ and CPF to Suppliers

--- app/suppliers/admin.py
+++ app/suppliers/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from . import models
 
 class SupplierAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description',)
-    search_fields = ('name',)
+    list_display = ('name', 'cnpj_cpf', 'description',)
+    search_fields = ('name', 'cnpj_cpf',)
 
 admin.site.register(models.Supplier, SupplierAdmin)
